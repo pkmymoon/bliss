17414c604cb8e0b8ab8fa48c2e51d134a32d2242
Mymoon PK <62475918+pkmymoon@users.noreply.github.com>
2024-03-07 15:39:24 +0300

    iage update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
       <main className="flex min-h-screen flex-col w-full items-center justify-between p-5">
           <Solution
-              imageOne="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg"
-              imageTwo="https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg"
+              imageOne="/images/clean5.png"
+              imageTwo="/images/garbagesA.png"
               likes="345"
               location="ldasflkjsdfh"
               title="Tree"
